4317135e4e06e86cf92dd27b3d4defef6e29e8c8
Kevin Gleason <kaagleason@gmail.com>
2014-04-01 04:20:04 -0400

    Fixed the Deque instantiation

--- MyJavaConsoleApp/src/LLReverserC.java
+++ MyJavaConsoleApp/src/LLReverserC.java
@@ -1,37 +1,38 @@
-
+import java.lang.*;
 import java.util.*;
-import org.apache.http.impl.client.*;
 public class LLReverserC<I> implements LLReverser<I>
 {
-	//instance variables
-	private LinkedList<I> ll;
-	private Deque<Integer> dq = new Deque<Integer>();
-	
-	LLReverserC(){
-		this.ll = new LinkedList<I>();
-		this.dq = new Deque<I>();
-	}
-	
-	public void push(I info){
-		this.ll.push(info);
-	}
+ //instance variables
+ private LinkedList<I> ll;
+ private Deque<I> dq;
+ 
+ LLReverserC(){
+  this.ll = new LinkedList<I>();
+  this.dq = new ArrayDeque<I>();
+ }
+ 
+ public void push(I info){
+  this.ll.push(info);
+ }
 
-	public I pop(){
-		return this.ll.pop();
-	}
+ public I pop(){
+  return this.ll.pop();
+ }
 
-	
-	public LLReverser reverse(){
-		return new LLReverserC<I>();
-	}
-	
-	public static void main(String[] args){
-		LLReverser<Integer> llr = new LLReverserC<Integer>();
-		llr.push(5);
-		llr.push(10);
-		llr.push(15);
-		llr.push(4);
-		llr.toString();
-		
-	}
+ 
+ public LLReverser reverse(){
+     while (!this.ll.isEmpty()){
+         
+     }
+ }
+ 
+ public static void main(String[] args){
+  LLReverser<Integer> llr = new LLReverserC<Integer>();
+  llr.push(5);
+  llr.push(10);
+  llr.push(15);
+  llr.push(4);
+  llr.toString();
+  
+ }
 }
