86b5b85a8afc72ef82c1a78ff5f19832f7b39dcb
Kevin Gleason <kaagleason@gmail.com>
2014-04-02 15:43:17 -0400

    Fixed count8

--- RecursionPractice/src/Main.java
+++ RecursionPractice/src/Main.java
@@ -32,7 +32,7 @@ public class Main
 	
 	public static int count8(int n){
 		if (n%10==0) return 0;
-		if (n%10==8 && n/10%10==8) return 2+count8(n/10);
+		if (n%10==8 && n/10%10==8) return 3+count8(n/10);
 		if (n%10==8) return 1 + count8(n/10);
 		return count8(n/10);
 	}
