6a0cb032c57b92fab5ccb8e0e73e1d6f5847f51e
GleasonK <KAAGleason@Gmail.com>
2014-08-28 23:17:03 -0400

    added tip calculator

---
+++ AndroidApp/app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.mycompany.myapp;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+	
+	public void doCalculate(View view){
+		EditText totalText = (EditText) findViewById(R.id.mainTotal);
+		EditText tipText = (EditText) findViewById(R.id.mainTip);
+		
+		TextView totalView = (TextView) findViewById(R.id.mainTotalView);
+		TextView tipView = (TextView) findViewById(R.id.mainTipView);
+		TextView totalWithTipView = (TextView) findViewById(R.id.mainTotalWithTipView);
+		
+		double total = Double.valueOf(totalText.getText().toString());
+		double tipPercent = Double.valueOf(tipText.getText().toString());
+		
+		double tip = total * (tipPercent/100);
+		double toPay = total + tip;
+		
+		totalText.setText("");
+		tipText.setText("");
+		
+		totalView.setText(toDollar(total));
+		tipView.setText(toDollar(tip));
+		totalWithTipView.setText(toDollar(toPay));
+		
+		
+    }
+	
+	public String toDollar(double value){
+		return String.format("$%.2f",value);
+	}
+}
